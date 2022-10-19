b4549f4faa5e6a9619fb24c47bf102b45a774c83
tom.mansion <tomansion@yahoo.fr>
2022-10-19 18:21:02 +0200

    1.1.8 fixed assets not included on setup

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup
 
 setup(
     name='factorioBlueprintAnalyser',
-    version='1.1.8',
+    version='1.1.9',
     description="A python library analyse Factorio Blueprints and find bottlenecks.",
     url="https://github.com/tomansion/factorio_blueprint_analyser_app/",
     author="Tom Mansion",
     license="MIT License",
     install_requires=["termcolor==2.0.1", "PyYAML==5.4.1", "pyvis==0.3.0"],
-    packages=['factorio_blueprint_analyser']
+    package_data={
+        "factorio_blueprint_analyser": [
+            "assets/factorio_raw/factorio_raw_min.json"
+        ]
+    },
+    packages=['factorio_blueprint_analyser'],
 )
