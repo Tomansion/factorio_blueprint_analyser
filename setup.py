85d3f2dc3d27b10dd6c5c17dd7b751c77164afb8
tom.mansion <tomansion@yahoo.fr>
2022-10-19 16:31:44 +0200

    1.1.2 fixed build script

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ parent_dir = Path(__file__).resolve().parent
 
 setup(
     name='factorioBlueprintAnalyser',
-    version='1.1.1',
+    version='1.1.2',
     description="A python library analyse Factorio Blueprints and find bottlenecks.",
     url="https://github.com/tomansion/factorio_blueprint_analyser_app/",
     author="Tom Mansion",
     license="MIT License",
     install_requires=parent_dir.joinpath(
         "requirements.txt").read_text().splitlines(),
-    packages=['factorioBlueprintAnalyser'],
+    packages=['factorio_blueprint_analyser'],
 )
