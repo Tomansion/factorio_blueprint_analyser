355c77bcdc842ca984af4841db44937e173e1d1f
tom.mansion <tomansion@yahoo.fr>
2022-10-19 16:51:31 +0200

    1.1.4 fixed build script

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 from setuptools import setup
-from pathlib import Path
-
-parent_dir = Path(__file__).resolve().parent
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
     name='factorioBlueprintAnalyser',
-    version='1.1.3',
+    version='1.1.4',
     description="A python library analyse Factorio Blueprints and find bottlenecks.",
     url="https://github.com/tomansion/factorio_blueprint_analyser_app/",
     author="Tom Mansion",
     license="MIT License",
-    install_requires=parent_dir.joinpath(
-        "requirements.txt").read_text().splitlines(),
-    packages=['factorio_blueprint_analyser'],
+    install_requires=required,
+    packages=['factorio_blueprint_analyser']
 )
